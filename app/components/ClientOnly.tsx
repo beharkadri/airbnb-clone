ad3c344c715de5f4441867979842acb9bbfb03a3
Behar Kadri <kadri.behar@gmail.com>
2023-05-13 02:34:50 +0200

    Fixed potential hydration errors for client side components

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+interface ClientOnlyProps {
+	children: React.ReactNode;
+}
+
+const ClientOnly: React.FC<ClientOnlyProps> = ({ children }) => {
+	const [hasMounted, setHasMounted] = useState(false);
+
+	useEffect(() => {
+		setHasMounted(true);
+	}, []);
+
+	return hasMounted ? <>{children}</> : null;
+};
+
+export default ClientOnly;
