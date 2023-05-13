3d92a8ce29926f86d86fb68a6e92263aef8154c9
Behar Kadri <kadri.behar@gmail.com>
2023-05-13 13:13:06 +0200

    Map component and finished Location step for listing creation

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -8,6 +8,7 @@ import Heading from "../Heading";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 enum STEPS {
 	CATEGORY = 0,
@@ -45,6 +46,12 @@ const RentModal = () => {
 
 	const category = watch("category");
 	const location = watch("location");
+
+	const Map = useMemo(
+		() => dynamic(() => import("../Map"), { ssr: false }),
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[location]
+	);
 
 	const setCustomValue = (id: string, value: any) => {
 		setValue(id, value, {
@@ -108,7 +115,7 @@ const RentModal = () => {
 		</div>
 	);
 
-	if (step === STEPS.DESCRIPTION) {
+	if (step === STEPS.LOCATION) {
 		bodyContent = (
 			<div className="flex flex-col gap-8">
 				<Heading
@@ -119,6 +126,7 @@ const RentModal = () => {
 					value={location}
 					onChange={(value) => setCustomValue("location", value)}
 				/>
+				<Map center={location?.latlng} />
 			</div>
 		);
 	}
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Nunito } from "next/font/google";
 import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
+import RentModal from "./components/modals/RentModal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default async function RootLayout({
 					<ToasterProvider />
 					<LoginModal />
 					<RegisterModal />
+					<RentModal />
 					<Navbar currentUser={currentUser} />
 				</ClientOnly>
 				{children}
