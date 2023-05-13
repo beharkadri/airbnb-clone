e9b0f49bd89402a35c066a0e239e8a748cf75602
Behar Kadri <kadri.behar@gmail.com>
2023-05-13 19:18:26 +0200

    Finished My properties page and delete listing functionality

--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import getCurrentUser from "../actions/getCurrentUser";
 import ClientOnly from "../components/ClientOnly";
 import EmptyState from "../components/EmptyState";
-import getReservations from "../actions/getReservations";
 import ReservationsClient from "./ReservationsClient";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
 
 const ReservationsPage = async () => {
 	const currentUser = await getCurrentUser();
